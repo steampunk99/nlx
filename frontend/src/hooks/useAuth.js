2d212b7899559d0c400673bb8a466d5bfbe87ee7
steampunk99 <luk23bonnie8@gmail.com>
2025-01-08 17:09:21 +0300

    ui fixes

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -90,7 +90,7 @@ export function useAuth() {
     },
     onSuccess: (data) => {
       try {
-      if(userPackage.userPackage.status === 'ACTIVE') {
+      if(userPackage.userPackage) {
         toast.success('Welcome back')
         setTimeout(()=>navigate('/dashboard'),3000)
       }

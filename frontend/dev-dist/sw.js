d773d7ead8eff472bd3e21ab934733985b9ec054
c-137 <loulater99@gmail.com>
2025-08-08 18:37:38 +0300

    activation page

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qnrdutidkc"
+    "revision": "0.3ilkb7mg9a8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
